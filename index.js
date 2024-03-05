ee0ea0d3d4d9259a438e7bf0ce0e2c400c189451
RuZlan <ruslantimonkin281@gmail.com>
2024-03-05 23:33:21 +0300

    cors

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://employees-frontend-bice.vercel.app/"
+  ]
+}));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
