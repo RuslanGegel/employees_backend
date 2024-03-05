53be82f2b11f83d58eb8d595287667684a351c6d
RuZlan <ruslantimonkin281@gmail.com>
2024-03-05 22:51:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", (opts) => {
+  return opts.res.send("Express server");
+});
+app.use("/api/user", require("./routes/users"));
+app.use("/api/employees", require("./routes/employees"));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, (err, res) => {
+  if (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  } else {
+    console.log("[INFO] Server Running on port:", port);
+  }
+});
+
+module.exports = app;
